d3a707846fae8b8c49de771bed45ed22480e52d4
root <root@magento2>
2015-06-26 16:34:21 +0000

    init module

---
+++ Block/Hello.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MDL
+ * Date: 26/06/15
+ * Time: 16:59
+ */
+
+namespace MDN\MyModule\Block;
+
+class Hello extends \Magento\Framework\View\Element\Template  {
+
+}
---
+++ Controller/HelloController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace MDN\MyModule\Controller;
+
+class HelloController extends \Magento\Framework\App\Action\Action {
+
+    public function helloAction() {
+        die('Hello World !');
+    }
+}
